bed34fc14fb698464b4f542a65dae55c292f7e31
backwardspy <backwardspy@pigeon.life>
2023-05-25 20:02:38 +0100

    feat: configure python tools in null-ls
    currently configures black, isort, and mypy.

--- lua/pigeon_conf/lang/python.lua
+++ lua/pigeon_conf/lang/python.lua
@@ -12,4 +12,24 @@ return {
       },
     },
   },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+
+      local sources = {
+        nls.builtins.formatting.black,
+        nls.builtins.formatting.isort,
+        nls.builtins.diagnostics.mypy,
+      }
+
+      for _, source in ipairs(sources) do
+        if vim.fn.executable(source._opts.command) == 1 then
+          opts.sources[#opts.sources + 1] = source
+        end
+      end
+
+      return opts
+    end,
+  },
 }
